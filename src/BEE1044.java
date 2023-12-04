3daec0fc2f2b4bc84843d68edb8cf8df8f93f4cc
Matheus <zedroguinha853@gmail.com>
2023-12-03 22:44:58 -0300

    Estruturas condicionais

--- src/BEE1044.java
+++ src/BEE1044.java
@@ -1,7 +1,14 @@
 import  java.util.Scanner;
-public class BEE1044 {
+public class BEE1044{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int A,B;
+        A = sc.nextInt();
+        B = sc.nextInt();
+        if (B%A==0){
+            System.out.println("Sao Multiplos");
+        }else
+            System.out.println("Nao sao Multiplos");
 
 
     }
